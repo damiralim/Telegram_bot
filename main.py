7715e2ebbce91b268fdcbe42de4fa234059e4d2e
damir <YWorker@yandex.ru>
2022-05-10 20:58:02 +0300

    shelty_bot - 2 stage

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-# 5384137466:AAFWagd9CXO-oX9jyntG898s1Cf4G5yE5UM
-import requests, telebot
+import requests, telebot, os
 
-token = '5384137466:AAFWagd9CXO-oX9jyntG898s1Cf4G5yE5UM'
+os.environ['TOKEN'] = '5384137466:AAFWagd9CXO-oX9jyntG898s1Cf4G5yE5UM'
+token = os.getenv('TOKEN')
 
 shelty_bot = telebot.TeleBot(token)
 
-@shelty_bot.message_handler(commands=['helloworld'])
+@shelty_bot.message_handler(commands=['hello-world'])
 def start_message(message):
     shelty_bot.send_message(message.chat.id,'Привет, юзер')
 
@@ -13,6 +13,8 @@ def start_message(message):
 def get_text_messages(message):
     if message.text == 'Привет':
         shelty_bot.send_message(message.from_user.id, 'Привет, как тебя зовут?')
+    elif message.text == 'Пока':
+      shelty_bot.send_message(message.from_user.id, 'Привет, как тебя зовут?')
 
 
 shelty_bot.infinity_polling()
